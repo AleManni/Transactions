9a31ff47c88c2c27f8697729b5f2f07d788785d8
Alessandro Manni <ale_manni@icloud.cm>
2018-08-25 17:32:45 +0100

    Some notes editing

--- BudTransactions/Modules/Representable.swift
+++ BudTransactions/Modules/Representable.swift
@@ -10,7 +10,7 @@ import Foundation
 
 /**
  Representable structures are the output of the Presenters to the View interfaces.
- They contain the data binding and transformation logic from one or many Domain Model(s) to a single struct ready to be displayed by the View without any further transformation
+ They contain the data binding and transformation logic from one or many Domain Model(s) to a single struct ready to be displayed by the View without any further transformation.
  */
 public protocol Representable {
   init?(model: DomainModel?...)
